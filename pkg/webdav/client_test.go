394116cebca878f6400a77a0209eb7e4a47d21a8
Elnus <sunle8@sohu.com>
2024-04-19 09:01:06 +0000

    修改单元测试

--- pkg/webdav/client_test.go
+++ pkg/webdav/client_test.go
@@ -4,10 +4,22 @@ import (
 	"context"
 	"fmt"
 	"testing"
+
+	"github.com/emersion/go-webdav"
 )
 
 // path
 func TestWebdav(t *testing.T) {
-	client, _ := CreateClient(CreatHttpClientWithAuth(&httpClient{}, "", ""), "")
+	//client, _ := CreateClient(CreatHttpClientWithAuth(&httpClient{}, "", ""), "")
+	client := InitClient(&httpClient{}, "", "", "")
+
+	// readDir
 	fmt.Println(client.ReadDir(context.Background(), "", false))
+
+	// move
+	//client.Move(context.Background(), "/via/settings.txt", ".settings.txt", &webdav.MoveOptions{NoOverwrite: true})
+
+	// copy
+	err := client.Copy(context.Background(), "", "", &webdav.CopyOptions{})
+	fmt.Println(err)
 }
