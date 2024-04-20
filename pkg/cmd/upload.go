55244b56fb618bee56da3bae9ce67d844fd01fbc
Elnus <sunle8@sohu.com>
2024-04-20 17:15:18 +0000

    新增上传文件代码

--- pkg/cmd/upload.go
+++ pkg/cmd/upload.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
 
+	"github.com/emersion/go-webdav"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +15,38 @@ var uploadCmd = &cobra.Command{
 	Short: "Upload file to webdav",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("is upload")
+		ctx, cancel := context.WithTimeout(context.Background(), vars.timeout)
+		defer cancel()
+
+		items, err := webdav.LocalFileSystem("/").ReadDir(ctx, vars.localDir, vars.recursive)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range items {
+			switch v.IsDir {
+			case true:
+				fmt.Println("this is a fold")
+			case false:
+				localFile, err := webdav.LocalFileSystem("/").Open(ctx, v.Path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				data, err := io.ReadAll(localFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				_, err = vars.Client.Stat(ctx, v.Path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				r, err := vars.Client.Create(ctx, v.Path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer r.Close()
+				r.Write(data)
+			}
+		}
 	},
 }
 
