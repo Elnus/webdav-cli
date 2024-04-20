e2c0a6adf43bacd816942a88f738ed48225072a9
Elnus <sunle8@sohu.com>
2024-04-20 02:41:07 +0000

    添加open、stat、mkdir等多种测试案例

--- pkg/webdav/client_test.go
+++ pkg/webdav/client_test.go
@@ -3,23 +3,68 @@ package webdav
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/emersion/go-webdav"
 )
 
 // path
-func TestWebdav(t *testing.T) {
+func TestRead(t *testing.T) {
 	//client, _ := CreateClient(CreatHttpClientWithAuth(&httpClient{}, "", ""), "")
 	client := InitClient(&httpClient{}, "", "", "")
-
-	// readDir
 	fmt.Println(client.ReadDir(context.Background(), "", false))
+}
 
-	// move
-	//client.Move(context.Background(), "/via/settings.txt", ".settings.txt", &webdav.MoveOptions{NoOverwrite: true})
+func TestCreate(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	file, _ := client.Create(context.Background(), "")
+	byte, _ := os.ReadFile("")
+	file.Write(byte)
+	defer file.Close()
+}
 
-	// copy
+func TestFindCurrentUserPrincipal(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	fmt.Println(client.FindCurrentUserPrincipal(context.Background()))
+}
+
+func TestRemove(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	err := client.RemoveAll(context.Background(), "")
+	fmt.Println(err)
+}
+
+func TestStat(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	fileInfo, _ := client.Stat(context.Background(), "")
+	fmt.Println(fileInfo.Path)
+}
+
+func TestOpen(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	file, _ := client.Open(context.Background(), "")
+	data, _ := io.ReadAll(file)
+	defer file.Close()
+	//fmt.Println(string(data))
+	osFile, _ := os.Create(".")
+	osFile.Write(data)
+}
+
+func TestCopy(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
 	err := client.Copy(context.Background(), "", "", &webdav.CopyOptions{})
+	fmt.Println(err)
+}
+
+func TestMove(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	client.Move(context.Background(), "", "", &webdav.MoveOptions{NoOverwrite: true})
+}
+
+func TestMkdir(t *testing.T) {
+	client := InitClient(&httpClient{}, "", "", "")
+	err := client.Mkdir(context.Background(), "")
 	fmt.Println(err)
 }
